ec1dc48b242596cd10fe07dac400ae7ebd76a465
Eastyn <eastynwork@gmail.com>
2023-04-02 23:23:14 +0900

    Tried a new thing I learned that you have to create another object and update it to update former object, but didn't worked out

--- src/InputTest.js
+++ src/InputTest.js
@@ -6,16 +6,30 @@ function InputTest() {
 	const onChange = e => {
 		setText(e.target.value);
 	};
-
-	const onReset = () => {
+	const onReset = e => {
 		setText('');
 	};
+	// const [inputs, setInputs] = useState({ name: '' });
+	// const { name } = inputs;
+
+	// const onChange = e => {
+	// 	const { value, name } = e.target;
+	// 	setInputs({
+	// 		...inputs, // spread 문법
+	// 		[name]: value,
+	// 	});
+	// };
+
+	// const onReset = () => {
+	// 	setInputs({ name: '' });
+	// };
 	return (
 		<div>
-			<input onChange={onChange} value={text} />
+			<input placeholder="이름" onChange={onChange} value={text} />
 			<button onClick={onReset}> 초기화 </button>
 			<div>
-				<b> 값 : {text}</b>
+				<b> 값 : {text} </b>
+				{/* {name} */}
 			</div>
 		</div>
 	);
