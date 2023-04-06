51d78e22aa4738bad6adb99a4d478092cc6e9e9e
Eastyn <eastynwork@gmail.com>
2023-04-06 23:45:42 +0900

    Add Firebase.js to use it soon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import TodaysTreats from './TodaysTreats';
 // import InputTreats from './InputTreats';
-
+import firebaseConfig from './Firebase';
+console.log(firebaseConfig);
 function App() {
 	return (
 		<div className="App">
---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyAaMZxBtvXDtk6bLSeygu3AEttH_nO1vCM',
+	authDomain: 'treat-nuri-kindly.firebaseapp.com',
+	projectId: 'treat-nuri-kindly',
+	storageBucket: 'treat-nuri-kindly.appspot.com',
+	messagingSenderId: '655714062533',
+	appId: '1:655714062533:web:98f4e249d1e3f1ffad20ac',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
