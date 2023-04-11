3d09837ec723cb94f5e7b449cf663ba89e2239f9
Eastyn <eastynwork@gmail.com>
2023-04-11 22:58:22 +0900

    Tried to add function on the button to upload the data but failed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import TodaysTreats from './TodaysTreats';
 // import InputTreats from './InputTreats';
 import firebaseConfig from './Firebase';
-import { Database } from 'firebase/database';
-console.log(Database);
+
 function App() {
 	return (
 		<div className="App">
--- src/TodaysTreats.js
+++ src/TodaysTreats.js
@@ -1,30 +1,51 @@
 import React from 'react';
 import InputTreats from './InputTreats';
+import database from './Firebase';
+import { getDownloadURL, ref, child, push, update } from 'firebase/database';
 
-const TodaysTreats = () => {
+console.log(database);
+
+function updateNewDailyTreats(date, dailyTreats) {
+	// A post entry.
+	const dailyTreatsData = {
+		date: date,
+		dailyTreats: dailyTreats,
+	};
+
+	// Get a key for a new Post.
+	const newDailyTreatsKey = push(child(ref(database), 'dailyTreats')).key;
+
+	// Write the new post's data simultaneously in the posts list.
+	const updates = {};
+	updates['/nuri/' + newDailyTreatsKey] = postData;
+
+	return update(ref(database), updates);
+}
+function setDate() {
+	let now = new Date();
+	let todayYear = now.getFullYear();
+	let todayMonth = now.getMonth() + 1;
+	let todayDate = now.getDate();
+	return todayYear + '.' + todayMonth + '.' + todayDate;
+}
+
+function TodaysTreats() {
 	// const [treats, setTreats] = useState('');
 
-	function todayIs() {
-		let now = new Date();
-		let todayYear = now.getFullYear();
-		let todayMonth = now.getMonth() + 1;
-		let todayDate = now.getDate();
-		return todayYear + '.' + todayMonth + '.' + todayDate;
-	}
 	// const onChange = e => {
 	// 	setTreats(e.target.value);
 	// };
 	return (
 		<div id="container">
 			<div>
-				<h2>{todayIs()}</h2>
+				<h2>{setDate()}</h2>
 				{/* <p> {treats} </p> */}
 			</div>
 			{/* <input placeholder="간식" onChange={onChange} value={treats} /> */}
 			<InputTreats />
-			<button id="Btn-submit-treats"> Update </button>
+			<button id="Btn-submit-treats">Update</button>
 		</div>
 	);
-};
+}
 
 export default TodaysTreats;
