17fc83ccdd1062731cd07310e66b93ee0d91f5f4
Eastyn <eastynwork@gmail.com>
2023-05-01 23:12:56 +0900

    Learn how to setDoc() via FireStore

--- src/FireStore.js
+++ src/FireStore.js
@@ -1,12 +1,29 @@
-import { getDoc, doc } from 'firebase/firestore';
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from './Firebase';
+const testerDocumentRef = doc(db, 'nuri123/2023-05-01');
 
-export async function getComments(nickname) {
-	const comments = doc(db, 'comments', nickname);
-	const commentsSnapshot = await getDoc(comments);
-
-	if (!commentsSnapshot.exists()) {
-		return null;
-	}
-
-	return commentsSnapshot.data();
+function FireStoreTester() {
+	// setDoc(doc(db, 'nuri123', '2023-04-30'), {
+	// 	createdAt: '2023-05-01',
+	// 	treatList: [
+	// 		{
+	// 			createdAt: '04/30/2023, 11:27:05 PM',
+	// 			title: '사슴고기 육포 3알',
+	// 		},
+	// 		{
+	// 			createdAt: '04/30/2023, 11:27:37 PM',
+	// 			title: '소고기 육포 3알',
+	// 		},
+	// 		{
+	// 			createdAt: '04/30/2023, 11:27:41 PM',
+	// 			title: '소고기 개껌 1개',
+	// 		},
+	// 		{
+	// 			createdAt: '04/30/2023, 11:27:45 PM',
+	// 			title: '오리고기 개껌 1개',
+	// 		},
+	// 	],
+	// });
 }
+
+export default FireStoreTester;
